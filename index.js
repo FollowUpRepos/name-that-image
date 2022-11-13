64e42e542a0a80ed377d8041841603d6541f2c2a
James Newton <blackslate@lexogram.com>
2022-11-13 16:52:53 +0300

    Show three random images and names in random order
    * Add shuffle() and getRandomName() functions
    * Add an imageDiv and a nameDiv to the document body
    * Create nameArray to hold 3 names
    * For each of 3 random namesname:
      - Create an img element with the appropriate src
      - Append this img element as a child of imageDiv
      - Add the name to nameArray
    * Shuffle nameArray
    * Create a div element for each name and append it to nameDiv
    * Add stub for starting drag
    * Add CSS for the body, img, #name div and #names elements

--- index.js
+++ index.js
@@ -9,11 +9,65 @@ const names = [
   "toenails"
 ]
 
-const fileNames = names.map( name => {
-  return `/img/${name}.jpg`
+// Get three random images
+function getRandomName() {
+  const index = Math.floor(Math.random() * names.length)
+  return names.splice(index, 1)[0] // [ "name" ]
+}
+
+const imageDiv = document.createElement("DIV")
+const nameDiv  = document.createElement("DIV")
+nameDiv.id = "names"
+document.body.appendChild(imageDiv)
+document.body.appendChild(nameDiv)
+
+
+let nameArray = []
+for ( let ii = 0; ii < 3; ii += 1 ) {
+  const image = document.createElement("IMG")
+  const name = getRandomName()
+  const src = `/img/${name}.jpg`
+  image.src = src
+  imageDiv.appendChild(image)
+
+  // Remember the names for later
+  nameArray.push(name)
+}
+
+shuffle(nameArray)
+console.log("nameArray:", nameArray);
+
+nameArray.forEach( name => {
+  const div = document.createElement("DIV")
+
+  div.textContent = name
+  // div.addEventListener("mousedown", startingToDrag)
+
+  // The `dragstart` event requires a `draggable` property
+  // to be set on the element, otherwise it will not be
+  // triggered
+  div.draggable="true"
+  div.addEventListener("dragstart", startingToDrag)
+
+  nameDiv.appendChild(div)
 })
 
-const image = document.createElement("IMG")
-image.src = fileNames[0]
 
-document.body.appendChild(image)
+function startingToDrag(event) {
+  event.preventDefault()
+  console.log("event:", event);
+}
+
+
+
+function shuffle (a) {
+  let ii = a.length
+
+  while (ii) {
+    const jj = Math.floor(Math.random() * ii)
+    ii -= 1;
+    [a[ii], a[jj]] = [a[jj], a[ii]]
+  }
+
+  return a // for chaining
+}
